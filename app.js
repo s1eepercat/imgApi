0485cdc98d41ddea7044f99f60fbb6812befaee3
s1eepercat <jevlentjev@gmail.com>
2020-04-14 12:55:53 +0300

    test

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static('public'));
-app.listen(3000, function () { console.log("Server is on, port " + port + "."); });
-app.get('/', function (req, res) {
+app.listen(port, function () { console.log("Server is on, port " + port + "."); });
+app.get('*', function (req, res) {
     res.status(200).json({ ass: 'ass' });
 });
+// var express = require('express')
+// var path = require('path')
+// var serveStatic = require('serve-static')
+// var app = express()
+// app.use(serveStatic(path.join(__dirname, 'dist')))
+// var port = process.env.PORT || 8000
+// app.listen(port)
+// console.log('server started ' + port)
